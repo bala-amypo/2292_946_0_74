bfdcc754bf124a5b53700e35bc2112d0798683d1
bala-amypo <bala@amypo.in>
2025-12-16 08:17:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -9,6 +9,26 @@ public class Student{
     public long getId(){
         return Id;
     }
-
+    public void setId(long Id){
+        this.Id=Id;
+    }
+    public string getName(){
+        return name;
+    }
+    public void setName(){
+        this.name=name;
+    }
+    public string getEmail(){
+        return email;
+    }
+    public void setEmail(string Email){
+        this.email=email;
+    }
+    public float getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(float cgpa){
+        this.cgpa=cgpa;
+    }
 
 }
